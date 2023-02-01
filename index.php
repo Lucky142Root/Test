3db78036b71bab7325797d4a3ed66d9d7a2777f8
Lucky142Root <124116433+Lucky142Root@users.noreply.github.com>
2023-02-01 21:57:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
-<?php
-echo 'hello world';
-echo 'hello igor'?
+<<?php
+$x=5;
+echo $x;
+echo'<br/>';
+echo PHP_INT_MAX;
+$d_1=1.5;
+$d_2=9.8e5;
+$d_3=9.8e-5;
+echo "d_1=$d_1;d_2=$d_2;d_3=$d_3";
+$s='My string';
+echo $s;
+echo '<br/>';
+$b_0= false;
+$b_1=true;
+echo "b_0=$b_0;b_1=$b_1";
 ?>
-    
